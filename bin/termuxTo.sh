6d12217f03e7f42745a55bf0cebef5fa2e0fd39a
Michael Hoogkamer <michael.hoogkamer@gmail.com>
2018-12-01 18:18:10 +0200

    add WritingPrompts.org

--- bin/termuxTo.sh
+++ bin/termuxTo.sh
@@ -6,6 +6,7 @@ rsync -auv /home/michael/MEGA/ideamachine.org /home/michael/Yandex.Disk/termux
 rsync -auv /home/michael/MEGA/diary /home/michael/Yandex.Disk/termux/
 rsync -auv /home/michael/MEGA/notes.org /home/michael/Yandex.Disk/termux/
 rsync -auv /home/michael/MEGA/journal.org /home/michael/Yandex.Disk/termux/
+rsync -auv /home/michael/MEGA/WritingPrompts.org /home/michael/Yandex.Disk/termux/
 
 #Journal
 #rsync -auv /home/michael/MEGA/journal/ /home/michael/Yandex.Disk/termux/journal/
